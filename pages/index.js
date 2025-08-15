2b8d08fa32fb7fea349ba7eba3c7f2b5235d3245
Benjamin Chamlet <bchamlet@gmail.com>
2025-08-15 16:24:33 +1000

    Changing relative paths

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "components/Layout.js";
 import HeroImage from "../components/HeroImage";
 import SearchBar from "../components/SearchBar";
 import { useSegment } from "../lib/segment";
